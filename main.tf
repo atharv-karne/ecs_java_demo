b92570f51868578165a7ae1d3b7ace907990f3a1
atharv-karne <atharvdevops@gmail.com>
2024-09-16 10:51:29 +0530

    Monday

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "aws_iam_policy_attachment" "ecs_task_role_policy" {
 
 # Create IAM Instance Profile
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
-  name = "ecsInstanceProfileChanged7"
+  name = "ecsInstanceProfileChanged8"
   role = aws_iam_role.ecs_instance_role.name
 }
 
